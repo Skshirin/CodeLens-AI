2c7478236da48316147a23f710743b97c65a587a
Shaikh Shirin <skshirin007@gmail.com>
2026-08-30 20:36:03 +0530

    feat: implement authentication controller and configure OAuth2 and database settings

---
+++ backend/src/main/java/codeLens/backend/dto/UserResponse.java
@@ -0,0 +1,11 @@
+package codeLens.backend.dto;
+
+import java.util.UUID;
+
+public class UserResponse {
+
+    public UserResponse(UUID id, Long githubId, String githubUsername, String displayName, String avatarUrl) {
+        //TODO Auto-generated constructor stub
+    }
+    
+}
